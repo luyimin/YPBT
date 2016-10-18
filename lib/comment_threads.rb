9a9755d541dcf1012bd15e5256e8ec36154012f7
luyimin <b37582000@gmail.com>
2016-10-18 23:13:18 +0800

    Project_lib and pass all test

---
+++ lib/comment_threads.rb
@@ -0,0 +1,23 @@
+
+require_relative 'display_text'
+require_relative 'youtube_api'
+
+module VideoCommentThreads
+  #Main class to setup a LikedVideos
+  class CommentThreads
+    attr_reader :kind
+
+    def initialize(youtube_api, video_id:, api_key:)
+     @youtube_api = youtube_api
+     @video_id = video_id
+     @api_key = api_key
+     @kind = @youtube_api.comment_threads_info(@video_id)[0]['kind']
+    end
+
+    def comment_threads
+      return @comment_threads_items if @comment_threads_items
+    comment_threads = @youtube_api.comment_threads_info(@video_id)
+    @comment_threads = comment_threads
+    end
+  end
+ end
