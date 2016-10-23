ad105583c373e3888c3f297b7ace025b48a21e06
luyimin <b37582000@gmail.com>
2016-10-23 20:16:19 +0800

    Update videocommentthreads_spec2.rb

--- spec/videocommentthreads_spec2.rb
+++ spec/videocommentthreads_spec2.rb
@@ -2,10 +2,26 @@
 require_relative 'spec_helper.rb'
 
 describe 'VideoCommentThreads specifications' do
+  VCR.configure do |c|
+    c.cassette_library_dir = CASSETTES_FOLDER
+    c.hook_into :webmock
+
+    c.filter_sensitive_data('<API_KEY>') { CREDENTIALS[:api_key] }
+    c.filter_sensitive_data('<API_KEY_ESCAPED>') do
+      URI.escape(CREDENTIALS[:api_key])
+    end
+  end
+
   before do
+    VCR.insert_cassette CASSETTE_FILE, record: :new_episodes
+
     @youtube_api = VideoCommentThreads::YtApi.new(
       api_key: CREDENTIALS[:api_key]
     )
+  end
+
+  after do
+    VCR.eject_cassette
   end
 
   it 'should be able to create the display text of a video' do
