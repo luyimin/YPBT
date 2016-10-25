646136bb82bc99361726f659a5e1c346e4a1696e
luyimin <b37582000@gmail.com>
2016-10-25 21:35:55 +0800

    Update video.rb

--- lib/video.rb
+++ lib/video.rb
@@ -12,8 +12,6 @@ module YoutubeVideo
       video = @youtube_api.video_info(video_id)
       @title = video['title']
       @id = video_id
-      # @video_id = video_id
-      # @kind = @youtube_api.comment_threads_info(@video_id)[0]['kind']
     end
 
     def commentthreads
@@ -28,14 +26,5 @@ module YoutubeVideo
         )
       end
     end
-    # def comment_threads
-    #   return @comment_threads_items if @comment_threads_items
-    #   @comment_threads = @youtube_api.comment_threads_info(@video_id)
-    # end
-
-    # def self.find(youtube_api, video_id:)
-    #   comment_threads_data = youtube_api.comment_threads_info(video_id)
-    #   new(youtube_api, data: comment_threads_data)
-    # end
   end
 end
